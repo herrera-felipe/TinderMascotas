b14b65ea6fba941c87e1a07392c8601c0b74549d
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-01 22:54:16 -0300

    Modificado VotoRepositorio

--- src/test/java/com/practica/tinder/repositories/VotoRepositorio.java
+++ src/test/java/com/practica/tinder/repositories/VotoRepositorio.java
@@ -1,6 +1,10 @@
 package com.practica.tinder.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.practica.tinder.entitys.Voto;
@@ -8,4 +12,9 @@ import com.practica.tinder.entitys.Voto;
 @Repository
 public interface VotoRepositorio extends JpaRepository<Voto, String> {
 
+	@Query("SELECT c FROM Voto c WHERE c.mascotaOriginaVoto.id = :id ORDER BY c.fecha DESC")
+	public List<Voto> buscarVotosPropios(@Param("id") String id);
+	
+	@Query("SELECT c FROM Voto c WHERE c.mascotaRecibeVoto.id = :id ORDER BY c.fecha DESC")
+	public List<Voto> buscarVotosRecibidos(@Param("id") String id);
 }
