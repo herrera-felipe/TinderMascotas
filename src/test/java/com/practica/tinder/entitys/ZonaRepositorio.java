2c2203b140556df7589d67c0fd8e6ee77233b5aa
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-11-01 22:57:33 -0300

    ZonaRepositorio agregado al paquete correspondiente

--- src/test/java/com/practica/tinder/entitys/ZonaRepositorio.java
+++
@@ -1,5 +0,0 @@
-package com.practica.tinder.entitys;
-
-public class ZonaRepositorio {
-
-}
---
+++ src/test/java/com/practica/tinder/repositories/ZonaRepositorio.java
@@ -0,0 +1,9 @@
+package com.practica.tinder.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practica.tinder.entitys.Zona;
+
+public interface ZonaRepositorio extends JpaRepository<Zona, String> {
+
+}
